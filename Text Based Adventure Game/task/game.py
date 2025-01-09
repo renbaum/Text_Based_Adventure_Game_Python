fdc4140c79eecc2ea8d9395e894f494364b72f1a
renbaum <renbaum04@bluewin.ch>
2025-01-09 15:03:48 +0100

    Whats on the menu

---
+++ Text Based Adventure Game/task/game.py
@@ -0,0 +1,10 @@
+# write your code here
+from menu import MainMenu
+
+
+if __name__ == "__main__":
+
+    menu = MainMenu()
+    menu.do_game_loop()
+
+    print("Goodbye!")
---
+++ Text Based Adventure Game/task/menu.py
@@ -0,0 +1,35 @@
+
+
+class MainMenu:
+    def __init__(self):
+        self.show_title()
+        self.exit_game = False
+
+    def do_game_loop(self):
+        while not self.exit_game:
+            self.show_main_menu()
+            self.exit_game = self.get_main_input()
+
+
+    def show_main_menu(self):
+        print("1. Start a new game (START)")
+        print("2. Load your progress (LOAD)")
+        print("3. Quit the game (QUIT)")
+
+    def show_title(self):
+        print("***Welcome to the Journey to Mount Qaf***")
+        print()
+
+    def get_main_input(self):
+        while True:
+            choice = input().lower()
+            match choice:
+                case "1" | "start":
+                    print("Starting a new Game")
+                case "2" | "load":
+                    print("Loading your progress")
+                case "3" | "quit":
+                    break
+                case _:
+                    print("Unknown input! Please enter a valid one.")
+        return True
